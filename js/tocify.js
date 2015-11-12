aea6f557b4ebb20b85ee5b01e20b346fa3e1901d
Stefan Auditor <stefan.auditor@erdfisch.de>
2015-11-12 14:41:49 +0100

    Add the complete set of configuration options

--- js/tocify.js
+++ js/tocify.js
@@ -11,8 +11,26 @@ Drupal.behaviors.nodeDetailsSummaries = {
   attach: function (context) {
 
     var toc = $("#tocify").tocify({
-      selectors: "h2,h3,h4,h5",
-      context: '.node__content'
+      context: '.node__content',
+      selectors: 'h2,h3,h4,h5,h6',
+      showAndHide: true,
+      showEffect: 'slideDown',
+      showEffectSpeed: 'medium',
+      hideEffect: 'none',
+      hideEffectSpeed: 'medium',
+      smoothScroll: true,
+      scrollTo: 95,
+      showAndHideOnScroll: true,
+      highlightOnScroll: true,
+      highlightOffset: 40,
+      theme: 'none',
+      extendPage: true,
+      extendPageOffset: 100,
+      history: true,
+      hashGenerator: 'compact',
+      highlightDefault: true,
+      ignoreSelector: null,
+      scrollHistory: false
     });
 
   }
