b2f6f4f5c281397308a4b5acc873310fc6345fad
Stefan Auditor <stefan.auditor@erdfisch.de>
2017-05-03 14:59:54 +0200

    Issue #2875280 by sanduhrs: Fix settings labels and descriptions

--- src/Plugin/Block/TableOfContents.php
+++ src/Plugin/Block/TableOfContents.php
@@ -75,6 +75,7 @@ class TableOfContents extends BlockBase implements ContainerFactoryPluginInterfa
     $form['theme'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Theme'),
+      '#description' => $this->t('Choose the theme, e.g. "bootstrap", "jqueryui" or "none"'),
       '#default_value' => isset($this->configuration['_theme']) ? $this->configuration['_theme'] : $defaults->get('_theme'),
       '#maxlength' => 64,
       '#size' => 64,
@@ -83,6 +84,7 @@ class TableOfContents extends BlockBase implements ContainerFactoryPluginInterfa
     $form['context'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Context'),
+      '#description' => $this->t('Choose any valid jQuery selector, e.g. "body"'),
       '#default_value' => isset($this->configuration['_context']) ? $this->configuration['_context'] : $defaults->get('_context'),
       '#maxlength' => 64,
       '#size' => 64,
@@ -90,8 +92,8 @@ class TableOfContents extends BlockBase implements ContainerFactoryPluginInterfa
     );
     $form['selectors'] = array(
       '#type' => 'textfield',
-      '#title' => $this->t('selectors'),
-      '#description' => $this->t('selectors'),
+      '#title' => $this->t('Selectors'),
+      '#description' => $this->t('Each comma separated selector must be a header element, e.g. "h1,h2,h3"'),
       '#default_value' => isset($this->configuration['selectors']) ? $this->configuration['selectors'] : $defaults->get('selectors'),
       '#maxlength' => 64,
       '#size' => 64,
@@ -99,8 +101,8 @@ class TableOfContents extends BlockBase implements ContainerFactoryPluginInterfa
     );
     $form['show_and_hide'] = array(
       '#type' => 'textfield',
-      '#title' => $this->t('show_and_hide'),
-      '#description' => $this->t('show_and_hide'),
+      '#title' => $this->t('Show and hide'),
+      '#description' => $this->t('Should elements be shown and hidden, e.g. "true" or "false"'),
       '#default_value' => isset($this->configuration['show_and_hide']) ? $this->configuration['show_and_hide'] : $defaults->get('show_and_hide'),
       '#maxlength' => 64,
       '#size' => 64,
@@ -108,8 +110,8 @@ class TableOfContents extends BlockBase implements ContainerFactoryPluginInterfa
     );
     $form['show_effect'] = array(
       '#type' => 'textfield',
-      '#title' => $this->t('show_effect'),
-      '#description' => $this->t('show_effect'),
+      '#title' => $this->t('Show effect'),
+      '#description' => $this->t('Any of the jQuery show effects, e.g. "none", "fadeIn", "show", or "slideDown"'),
       '#default_value' => isset($this->configuration['show_effect']) ? $this->configuration['show_effect'] : $defaults->get('show_effect'),
       '#maxlength' => 64,
       '#size' => 64,
@@ -117,8 +119,8 @@ class TableOfContents extends BlockBase implements ContainerFactoryPluginInterfa
     );
     $form['show_effect_speed'] = array(
       '#type' => 'textfield',
-      '#title' => $this->t('show_effect_speed'),
-      '#description' => $this->t('show_effect_speed'),
+      '#title' => $this->t('Show effect speed'),
+      '#description' => $this->t('The time duration of the show effect, e.g. "slow", "medium", "fast", or any numeric number (milliseconds)'),
       '#default_value' => isset($this->configuration['show_effect_speed']) ? $this->configuration['show_effect_speed'] : $defaults->get('show_effect_speed'),
       '#maxlength' => 64,
       '#size' => 64,
@@ -126,8 +128,8 @@ class TableOfContents extends BlockBase implements ContainerFactoryPluginInterfa
     );
     $form['hide_effect'] = array(
       '#type' => 'textfield',
-      '#title' => $this->t('hide_effect'),
-      '#description' => $this->t('hide_effect'),
+      '#title' => $this->t('Hide effect'),
+      '#description' => $this->t('Any of the jQuery hide effects, e.g. "none", "fadeOut", "hide" or "slideUp"'),
       '#default_value' => isset($this->configuration['hide_effect']) ? $this->configuration['hide_effect'] : $defaults->get('hide_effect'),
       '#maxlength' => 64,
       '#size' => 64,
@@ -135,8 +137,8 @@ class TableOfContents extends BlockBase implements ContainerFactoryPluginInterfa
     );
     $form['hide_effect_speed'] = array(
       '#type' => 'textfield',
-      '#title' => $this->t('hide_effect_speed'),
-      '#description' => $this->t('hide_effect_speed'),
+      '#title' => $this->t('Hide effect speed'),
+      '#description' => $this->t('The time duration of the hide effect, e.g. "slow", "medium", "fast", or any numeric number (milliseconds)'),
       '#default_value' => isset($this->configuration['hide_effect_speed']) ? $this->configuration['hide_effect_speed'] : $defaults->get('hide_effect_speed'),
       '#maxlength' => 64,
       '#size' => 64,
@@ -144,8 +146,8 @@ class TableOfContents extends BlockBase implements ContainerFactoryPluginInterfa
     );
     $form['smooth_scroll'] = array(
       '#type' => 'textfield',
-      '#title' => $this->t('smooth_scroll'),
-      '#description' => $this->t('smooth_scroll'),
+      '#title' => $this->t('Smooth scroll'),
+      '#description' => $this->t('Animates the page scroll when specific table of content items are clicked and the page moves up or down, e.g. "true" or "false"'),
       '#default_value' => isset($this->configuration['smooth_scroll']) ? $this->configuration['smooth_scroll'] : $defaults->get('smooth_scroll'),
       '#maxlength' => 64,
       '#size' => 64,
@@ -153,8 +155,8 @@ class TableOfContents extends BlockBase implements ContainerFactoryPluginInterfa
     );
     $form['smooth_scroll_speed'] = array(
       '#type' => 'textfield',
-      '#title' => $this->t('smooth_scroll_speed'),
-      '#description' => $this->t('smooth_scroll_speed'),
+      '#title' => $this->t('Smooth scroll speed'),
+      '#description' => $this->t('The time duration of the animation'),
       '#default_value' => isset($this->configuration['smooth_scroll_speed']) ? $this->configuration['smooth_scroll_speed'] : $defaults->get('smooth_scroll_speed'),
       '#maxlength' => 64,
       '#size' => 64,
@@ -162,8 +164,8 @@ class TableOfContents extends BlockBase implements ContainerFactoryPluginInterfa
     );
     $form['scroll_to'] = array(
       '#type' => 'textfield',
-      '#title' => $this->t('scroll_to'),
-      '#description' => $this->t('scroll_to'),
+      '#title' => $this->t('Scroll to'),
+      '#description' => $this->t('The amount of space between the top of page and the selected table of contents item after the page has been scrolled'),
       '#default_value' => isset($this->configuration['scroll_to']) ? $this->configuration['scroll_to'] : $defaults->get('scroll_to'),
       '#maxlength' => 64,
       '#size' => 64,
@@ -171,8 +173,8 @@ class TableOfContents extends BlockBase implements ContainerFactoryPluginInterfa
     );
     $form['show_and_hide_on_scroll'] = array(
       '#type' => 'textfield',
-      '#title' => $this->t('show_and_hide_on_scroll'),
-      '#description' => $this->t('show_and_hide_on_scroll'),
+      '#title' => $this->t('Show and hide on scroll'),
+      '#description' => $this->t('Determines if table of content nested items should be shown and hidden while a user scrolls the page'),
       '#default_value' => isset($this->configuration['show_and_hide_on_scroll']) ? $this->configuration['show_and_hide_on_scroll'] : $defaults->get('show_and_hide_on_scroll'),
       '#maxlength' => 64,
       '#size' => 64,
@@ -180,8 +182,8 @@ class TableOfContents extends BlockBase implements ContainerFactoryPluginInterfa
     );
     $form['highlight_on_scroll'] = array(
       '#type' => 'textfield',
-      '#title' => $this->t('highlight_on_scroll'),
-      '#description' => $this->t('highlight_on_scroll'),
+      '#title' => $this->t('Highlight on scroll'),
+      '#description' => $this->t('Determines if table of content nested items should be highlighted while scrolling'),
       '#default_value' => isset($this->configuration['highlight_on_scroll']) ? $this->configuration['highlight_on_scroll'] : $defaults->get('highlight_on_scroll'),
       '#maxlength' => 64,
       '#size' => 64,
@@ -189,8 +191,8 @@ class TableOfContents extends BlockBase implements ContainerFactoryPluginInterfa
     );
     $form['highlight_offset'] = array(
       '#type' => 'textfield',
-      '#title' => $this->t('highlight_offset'),
-      '#description' => $this->t('highlight_offset'),
+      '#title' => $this->t('Highlight offset'),
+      '#description' => $this->t('The offset distance in pixels to trigger the next active table of contents item'),
       '#default_value' => isset($this->configuration['highlight_offset']) ? $this->configuration['highlight_offset'] : $defaults->get('highlight_offset'),
       '#maxlength' => 64,
       '#size' => 64,
@@ -198,8 +200,8 @@ class TableOfContents extends BlockBase implements ContainerFactoryPluginInterfa
     );
     $form['extend_page'] = array(
       '#type' => 'textfield',
-      '#title' => $this->t('extend_page'),
-      '#description' => $this->t('extend_page'),
+      '#title' => $this->t('Extend page'),
+      '#description' => $this->t('If a user scrolls to the bottom of the page and the page is not tall enough to scroll to the last table of contents item, then the page height is increased'),
       '#default_value' => isset($this->configuration['extend_page']) ? $this->configuration['extend_page'] : $defaults->get('extend_page'),
       '#maxlength' => 64,
       '#size' => 64,
@@ -207,8 +209,8 @@ class TableOfContents extends BlockBase implements ContainerFactoryPluginInterfa
     );
     $form['extend_page_offset'] = array(
       '#type' => 'textfield',
-      '#title' => $this->t('extend_page_offset'),
-      '#description' => $this->t('extend_page_offset'),
+      '#title' => $this->t('Extend page offset'),
+      '#description' => $this->t('How close to the bottom of the page a user must scroll before the page is extended'),
       '#default_value' => isset($this->configuration['extend_page_offset']) ? $this->configuration['extend_page_offset'] : $defaults->get('extend_page_offset'),
       '#maxlength' => 64,
       '#size' => 64,
@@ -216,8 +218,8 @@ class TableOfContents extends BlockBase implements ContainerFactoryPluginInterfa
     );
     $form['history'] = array(
       '#type' => 'textfield',
-      '#title' => $this->t('history'),
-      '#description' => $this->t('history'),
+      '#title' => $this->t('History'),
+      '#description' => $this->t('Adds a hash to the page url to maintain history'),
       '#default_value' => isset($this->configuration['history']) ? $this->configuration['history'] : $defaults->get('history'),
       '#maxlength' => 64,
       '#size' => 64,
@@ -225,8 +227,8 @@ class TableOfContents extends BlockBase implements ContainerFactoryPluginInterfa
     );
     $form['hash_generator'] = array(
       '#type' => 'textfield',
-      '#title' => $this->t('hash_generator'),
-      '#description' => $this->t('hash_generator'),
+      '#title' => $this->t("Hash generator"),
+      '#description' => $this->t("How the URL hash value get's generated"),
       '#default_value' => isset($this->configuration['hash_generator']) ? $this->configuration['hash_generator'] : $defaults->get('hash_generator'),
       '#maxlength' => 64,
       '#size' => 64,
@@ -234,8 +236,8 @@ class TableOfContents extends BlockBase implements ContainerFactoryPluginInterfa
     );
     $form['highlight_default'] = array(
       '#type' => 'textfield',
-      '#title' => $this->t('highlight_default'),
-      '#description' => $this->t('highlight_default'),
+      '#title' => $this->t('Highlight default'),
+      '#description' => $this->t("Set's the first table of content item as active if no other item is active"),
       '#default_value' => isset($this->configuration['highlight_default']) ? $this->configuration['highlight_default'] : $defaults->get('highlight_default'),
       '#maxlength' => 64,
       '#size' => 64,
@@ -243,8 +245,8 @@ class TableOfContents extends BlockBase implements ContainerFactoryPluginInterfa
     );
     $form['ignore_selector'] = array(
       '#type' => 'textfield',
-      '#title' => $this->t('ignore_selector'),
-      '#description' => $this->t('ignore_selector'),
+      '#title' => $this->t('Ignore selector'),
+      '#description' => $this->t('Elements that you do not want to be used to generate the table of contents'),
       '#default_value' => isset($this->configuration['ignore_selector']) ? $this->configuration['ignore_selector'] : $defaults->get('ignore_selector'),
       '#maxlength' => 64,
       '#size' => 64,
@@ -252,8 +254,8 @@ class TableOfContents extends BlockBase implements ContainerFactoryPluginInterfa
     );
     $form['scroll_history'] = array(
       '#type' => 'textfield',
-      '#title' => $this->t('scroll_history'),
-      '#description' => $this->t('scroll_history'),
+      '#title' => $this->t('Scroll history'),
+      '#description' => $this->t('Adds a hash to the page URL, to maintain history, when scrolling to a table of contents item'),
       '#default_value' => isset($this->configuration['scroll_history']) ? $this->configuration['scroll_history'] : $defaults->get('scroll_history'),
       '#maxlength' => 64,
       '#size' => 64,
